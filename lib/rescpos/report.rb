b2f5b5b605860de0f066ba513c9f825e9b86f522
Tower He <towerhe@gmail.com>
2011-08-29 02:51:25 +0800

    not ignore the newline for lines ending in %<>

--- lib/rescpos/report.rb
+++ lib/rescpos/report.rb
@@ -1,6 +1,7 @@
 module Rescpos
   class Report
     include ERB::Util
+    include ReportUtil
 
     def render(opts = {})
       template = opts[:file] ? File.read(opts[:file]) : opts[:template]
@@ -8,7 +9,7 @@ module Rescpos
         template_filename = self.class.to_s.gsub(/Report/, '').underscore
         template = File.read(File.join(Rescpos.configuration.template_path, "#{template_filename}.escpos.erb"))
       end
-      erb = ERB.new(template, 0, "%<>")
+      erb = ERB.new(template, 0)
       erb.result(binding)
     end
   end
